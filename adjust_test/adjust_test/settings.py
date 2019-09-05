24c4aebcac8ac09c36c180bd6cb8892b23fabe25
mechanicalmachine <anatolyplotnikov1@gmail.com>
2019-09-05 21:05:12 +0700

    db settings added

--- adjust_test/adjust_test/settings.py
+++ adjust_test/adjust_test/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'adjust_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'metrics_db',
+        'USER': 'metrics_user',
+        'PASSWORD': 'metrics_password',
+        'HOST': '127.0.0.1',
+        'PORT': '54322',
     }
 }
 
