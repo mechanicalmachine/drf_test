b88c6774c6882f7f55615a5c160dc66290ec2d43
mechanicalmachine <anatolyplotnikov1@gmail.com>
2019-09-05 23:26:00 +0700

    urls added

---
+++ adjust_test/api/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import MetricsViewSet
+
+
+router = DefaultRouter()
+router.register(r'metrics', MetricsViewSet, basename='metrics')
+urlpatterns = router.urls
