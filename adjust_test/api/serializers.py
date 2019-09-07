f6587b5d29ff00ed4069afc6888a88887b26a1a6
mechanicalmachine <anatolyplotnikov1@gmail.com>
2019-09-07 16:13:58 +0700

    dynamic group_by added

--- adjust_test/api/serializers.py
+++ adjust_test/api/serializers.py
@@ -3,7 +3,30 @@ from rest_framework import serializers
 from .models import Metric
 
 
-class MetricSerializer(serializers.ModelSerializer):
+class DynamicFieldsMetricSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        fields = kwargs['context']['request'].query_params.get('group_by', None)
+        super(DynamicFieldsMetricSerializer, self).__init__(*args, **kwargs)
+
+        if fields is not None:
+            fields = fields.split(',')
+            # default_fields = {'impressions_sum', 'clicks_sum', 'installs_sum', 'spend_sum', 'revenue_sum'}
+            allowed = set(fields) # | default_fields
+            existing = set(self.fields) # | default_fields
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
+class MetricSerializer(DynamicFieldsMetricSerializer):
+    impressions_sum = serializers.IntegerField()
+    clicks_sum = serializers.IntegerField()
+    installs_sum = serializers.IntegerField()
+    spend_sum = serializers.FloatField()
+    revenue_sum = serializers.FloatField()
+
     class Meta:
         model = Metric
-        fields = '__all__'
+        fields = ('date', 'channel', 'country', 'os', 'impressions_sum',
+                  'clicks_sum', 'installs_sum', 'spend_sum', 'revenue_sum')
+
+# TODO add serializer for requests without 'group_by'
--- adjust_test/api/views.py
+++ adjust_test/api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework import filters
@@ -7,9 +8,35 @@ from .serializers import MetricSerializer
 
 
 class MetricsViewSet(viewsets.ModelViewSet):
-    queryset = Metric.objects.all()
+    # TODO add allowed methods
+    # http://127.0.0.1:8000/api/metrics/?country=US&channel=adcolony&ordering=-spend&group_by=country,channel
+
+    def get_queryset(self):
+        queryset = Metric.objects.all()
+
+        group_by_str = self.request.query_params.get('group_by', None)
+
+        if not group_by_str:
+            return queryset
+
+        group_by_list = group_by_str.split(',')
+
+        # allowed_groups = {'date', 'channel', 'country', 'os'}
+        if set(group_by_list): # <= allowed_groups:
+            queryset = queryset.values(
+                *group_by_list
+            ).annotate(
+                impressions_sum=Sum('impressions'),
+                clicks_sum=Sum('clicks'),
+                installs_sum=Sum('installs'),
+                spend_sum=Sum('spend'),
+                revenue_sum=Sum('revenue')
+            )
+
+        return queryset
+
     serializer_class = MetricSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    # TODO add date_to and date_from to filterset_fields
-    filterset_fields = ['channel', 'country', 'os']
+    # # TODO add date_to and date_from to filterset_fields
+    filterset_fields = ['date', 'channel', 'country', 'os']
     ordering_fields = '__all__'
