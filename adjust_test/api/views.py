4125256be8b05a6439df844847bef2e4189753e8
mechanicalmachine <anatolyplotnikov1@gmail.com>
2019-09-07 17:55:00 +0700

    date filters added

--- adjust_test/api/views.py
+++ adjust_test/api/views.py
@@ -15,12 +15,14 @@ class MetricsViewSet(viewsets.ModelViewSet):
         queryset = Metric.objects.all()
 
         group_by_str = self.request.query_params.get('group_by', None)
+        date_from = self.request.query_params.get('date_from', None)
+        date_to = self.request.query_params.get('date_to', None)
 
-        if not group_by_str:
-            return queryset
+        group_by_list = []
+        if group_by_str:
+            group_by_list = group_by_str.split(',')
 
-        group_by_list = group_by_str.split(',')
-
+        # Group_by filters
         allowed_groups = {'date', 'channel', 'country', 'os'}
         if set(group_by_list) <= allowed_groups:
             queryset = queryset.values(
@@ -33,10 +35,19 @@ class MetricsViewSet(viewsets.ModelViewSet):
                 revenue_sum=Sum('revenue')
             )
 
+        # Dates filters
+        if date_from and date_to:
+            queryset = queryset.filter(date__range=[date_from, date_to])
+        elif date_from:
+            queryset = queryset.filter(date__gt=date_from)
+        elif date_to:
+            queryset = queryset.filter(date__lt=date_to)
+
+        # TODO add serializer if there are no group_by params
         return queryset
 
     serializer_class = MetricSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     # # TODO add date_to and date_from to filterset_fields
-    filterset_fields = ['date', 'channel', 'country', 'os']
+    filterset_fields = ['channel', 'country', 'os']
     ordering_fields = '__all__'
